30dda50293caaab5730ddd869855322619320aa0
ASK-DONE <knueppelalex@web.de>
2023-04-20 14:29:56 +0200

    Konto auto-überarbeitet.

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -21,16 +21,16 @@ public class App {
         Konto konto2 = new Konto();
         Konto konto3 = new Konto();
 
-        System.out.println("Anfangs-Kontostand von konto1: " + konto1.kontostand);
-        System.out.println("Anfangs-Kontostand von konto2: " + konto2.kontostand);
-        System.out.println("Anfangs-Kontostand von konto3: " + konto3.kontostand);
+        System.out.println("Anfangs-Kontostand von konto1: " + konto1.getKontostand());
+        System.out.println("Anfangs-Kontostand von konto2: " + konto2.getKontostand());
+        System.out.println("Anfangs-Kontostand von konto3: " + konto3.getKontostand());
 
-        konto1.kontostand *= 2;
-        konto2.kontostand *= 3;
-        konto3.kontostand *= 10;
+        konto1.setKontostand(konto1.getKontostand() * 2);
+        konto2.setKontostand(konto2.getKontostand() * 3);
+        konto3.setKontostand(konto3.getKontostand() * 10);
 
-        System.out.println("Aktueller Kontostand von konto1: " + konto1.kontostand);
-        System.out.println("Aktueller Kontostand von konto2: " + konto2.kontostand);
-        System.out.println("Aktueller Kontostand von konto3: " + konto3.kontostand);
+        System.out.println("Aktueller Kontostand von konto1: " + konto1.getKontostand());
+        System.out.println("Aktueller Kontostand von konto2: " + konto2.getKontostand());
+        System.out.println("Aktueller Kontostand von konto3: " + konto3.getKontostand());
     }
 }
--- com/cc/java/Konto.java
+++ com/cc/java/Konto.java
@@ -1,6 +1,14 @@
 package com.cc.java;
 
 public class Konto {
-    int kontostand = 10000;    
+    private int kontostand = 10000;
+
+    public int getKontostand() {
+        return kontostand;
+    }
+
+    public void setKontostand(int kontostand) {
+        this.kontostand = kontostand;
+    }    
 }
 
